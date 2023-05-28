fe5a5d8f2c59c7baf130df010b8643b4b79aade4
Replit user <>
2023-05-28 12:14:49 +0000

    Basic To do addition and review functions are added

--- main.py
+++ main.py
@@ -1,28 +1,37 @@
-class todolist:
+class Todo:
   pass 
 
-def new_task_entry():
-  pass
+todo_list= []
 
+# get text
+# store text in the todo list of strings
+def new_task_entry():
+  task = input ("Enter your new task: ")
+  todo_list.append(task)
+  print ("Task added successfully")
+
+# print out the todo list of strings
 def review_task():
-  pass
+  print("todo_list: ")
+  for task in todo_list:
+    print (task)
 
 #Selection of operations
 greeting = "Welcome to your to do list"
 print (greeting)
 
-operation=input("Please select the operation: \n1- Enter a new to do \n2- Review your current to do list \n3- Exit to do list\n")
-
-if operation == "1": 
-  print("Enter a new task")
-  new_task_entry()
-
-elif operation == "2":
-  print("Review your to do list") 
-  review_task()
+while True:
+  operation=input("Please select the operation: \n1- Enter a new to do task \n2- Review your current to do list \n3- Exit to do list\n")
   
-elif operation == "3":
-  print("Your to do list will be closed") 
-
-else:
-  print("Select a valid operation")
+  if operation == "1": 
+    new_task_entry()
+  
+  elif operation == "2":
+    review_task()
+  
+  elif operation == "3":
+    print("Your to do list will be closed") 
+    break
+  
+  else:
+    print("Select a valid operation")
